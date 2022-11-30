00610106f748926bd30aef801c05b4f17ad1fc29
Boma Iyowuna <itzbomzky@gmail.com>
2022-11-30 22:17:38 +0100

    scripts

---
+++ Scripts/Fishes.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fishes : MonoBehaviour
+{
+
+    public void jellySound(){
+        FindObjectOfType<AudioManager>().Play("test");
+   }
+    public  void whaleSound(){
+        FindObjectOfType<AudioManager>().Stop("test");
+        FindObjectOfType<AudioManager>().Play("test2");
+    }
+}
---
+++ Scripts/UI.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI : MonoBehaviour
+{
+     public GameObject Canvas;
+
+    public void OnPointerEnter(){
+        Canvas.SetActive(false);
+    }
+    public void OnPointerClick(){
+        
+    }
+    public void OnPointerExit(){
+        
+    }
+}
